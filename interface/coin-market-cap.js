4d7a8b6a9d55b63df26654aa0e7ba2409b9b3e05
Dean van Niekerk <deanvniekerk@gmail.com>
2018-05-06 21:18:35 +0200

    Updated to use coin market cap version 2 apis

--- interface/coin-market-cap.js
+++ interface/coin-market-cap.js
@@ -4,11 +4,40 @@ class CoinMarketCap {
 
     constructor(agent) {
         this.agent = agent;
-        this.apiRoot = 'https://api.coinmarketcap.com/v1';
+        this.apiRoot = 'https://api.coinmarketcap.com/v2';
     }
 
     getCoinTopList(start, limit) {
-        return this.agent.requests.get(`${this.apiRoot}/ticker/?start=${start}&limit=${limit}`);
+        return this.agent.requests.get(`${this.apiRoot}/ticker/?start=${start}&limit=${limit}`)
+            .then(data => {
+
+                let arr = [];
+                let coins = data.data;
+
+                for (let id in coins) {
+                    if (coins.hasOwnProperty(id)) {
+                        let c = coins[id];
+                        let coin = {
+                            name: c.name,
+                            symbol: c.symbol,
+                            rank: c.rank,
+                            priceUsd: c.quotes.USD.price,
+                            volumeUsd24h: c.quotes.USD.volume_24h,
+                            marketCapUsd: c.quotes.USD.market_cap,
+                            maxSupply: c.max_supply,
+                            availableSupply: c.circulating_supply,
+                            totalSupply: c.total_supply,
+                            percentChange1h: c.quotes.USD.percent_change_1h,
+                            percentChange24h: c.quotes.USD.percent_change_24h,
+                            percentChange7d: c.quotes.USD.percent_change_7d
+                        }
+
+                        arr.push(coin);
+                    }
+                }
+                return arr;
+
+            });
     }
 
 }
